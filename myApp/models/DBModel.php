bf3349afe8885973f4630af9c0b702f5bed41a25
Gina Agapescu <gina.agapescu@gmail.com>
2022-05-27 06:51:22 +0300

    change database name

--- myApp/models/DBModel.php
+++ myApp/models/DBModel.php
@@ -5,7 +5,7 @@ class DBModel
     protected $conn;
 
     public function db(){
-        $this->conn = new mysqli('localhost', 'myuser', '123', 'hanmade_market');
+        $this->conn = new mysqli('localhost', 'myuser', '123', 'market_handmade');
         if($this->conn->connect_error){
             die('Connection error!');
         }
