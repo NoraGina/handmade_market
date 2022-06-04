235ca1e99eb23b864a5d1214396a0802fabd4e0f
Gina Agapescu <gina.agapescu@gmail.com>
2022-06-04 09:59:41 +0300

    Modified deleteItemController to be able to edit the product

--- myApp/controllers/DeleteItemController.php
+++ myApp/controllers/DeleteItemController.php
@@ -9,10 +9,49 @@ class DeleteItemController extends AppController
         echo __FILE__;
         session_start();
         if (isset($_SESSION['user']) && isset($_SESSION['cart'])){
+            $id = $_GET['id'];
            $items = $_SESSION['cart'];
+           //$_SESSION['cart']= $items;
+           foreach($_SESSION['cart'] as $item){
+               if(is_array($_SESSION['cart'])){
+                $pId = $item['id'];
+                
+                // echo"<br>";
+                // echo"Items";
+                // echo"<pre>";
+                // var_dump($item);
+                // echo"</pre>";
+               
+                    if($item == $_SESSION['cart'][$id] && array_key_exists($id,$_SESSION['cart'])){
+                        
+                        echo"Session id" ;
+                     echo"<pre>";
+                     var_dump($_SESSION['cart'][$id]);
+                     echo"</pre>";
+                    $productModel = new ProductsModel;
+                   $product = $productModel->getProductById($pId);
+                
+                    $productQuantity = floatval($product['quantity']);
+                    $itemQuantity = floatval($item['itemQuantity']);
+                    $type = $product['type'];
+                    if($type == "Pe stoc"){
+                        $updateQuantity =  $productQuantity+$itemQuantity;
+                        $productModel->updateQuantity($updateQuantity, $pId);
+                    }
+                    
+                }
+                        
+               }
+           
+           
             unset($_SESSION['cart'][$id]);
-            header("Location:../cart");
-            }else{
+            header("Refresh:7;url=../cart");
+            
+        }
+       
+            
+            //header("Location:../cart");
+    }else{
                 echo"Something";
             }
 
